769b296fe4fd10f0be37904ec38696fd1f4d8e20
Robb Lovell <robblovell@gmail.com>
2016-09-08 09:09:31 -0700

    Refactor RTCView to not use BaseComponentPropTypes, applyNativeMethods, and StyleSheetPropType.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import RTCSessionDescription from './RTCSessionDescription';
 import MediaStream from './MediaStream';
 import MediaStreamTrack from './MediaStreamTrack';
 import getUserMedia from './getUserMedia';
-import RTCView from './RTCView/RTCView';
+import RTCView from './RTCView';
 
 module.exports = {
   RTCPeerConnection,
